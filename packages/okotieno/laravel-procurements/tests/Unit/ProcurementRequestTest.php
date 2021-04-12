0cf0316865f70465bcec92865f06fb92b9e25124
Owen Kelvin <owen@furahasolutions.tech>
2021-04-12 04:57:02 +0300

    user can approve procurement request

--- packages/okotieno/laravel-procurements/tests/Unit/ProcurementRequestTest.php
+++ packages/okotieno/laravel-procurements/tests/Unit/ProcurementRequestTest.php
@@ -49,4 +49,47 @@ class ProcurementRequestTest extends TestCase
     $this->actingAs($this->user, 'api')->postJson('api/procurements/requests', $procurementRequest)
       ->assertStatus(200);
   }
+  /**
+   * @test
+   * @group procurement
+   * @group procurement-request
+   * @group post-request
+   */
+  public function unauthenticated_users_cannot_approve_procurement_request()
+  {
+    $procurement = ProcurementRequest::factory()->create();
+    $this->postJson('/api/procurements/requests/pending-approval', ['approve' => true, 'procurement_request_id' => $procurement->id])
+      ->assertStatus(401);
+  }
+
+  /**
+   * @test
+   * @group procurement
+   * @group procurement-request
+   * @group post-request
+   */
+  public function unauthorised_users_cannot_approve_procurement_request()
+  {
+    $procurement = ProcurementRequest::factory()->create();
+    $this->actingAs($this->user, 'api')
+      ->postJson('/api/procurements/requests/pending-approval', ['approve' => true, 'procurement_request_id' => $procurement->id])
+      ->assertStatus(403);
+  }
+
+  /**
+   * @test
+   * @group procurement
+   * @group procurement-request
+   * @group post-request
+   */
+  public function authorised_users_can_approve_procurement_request()
+  {
+    Permission::factory()->state(['name' => 'approve procurement request'])->create();
+    $this->user->givePermissionTo('approve procurement request');
+    $procurement = ProcurementRequest::factory()->create();
+    $this->actingAs($this->user, 'api')
+      ->postJson('/api/procurements/requests/pending-approval', ['approve' => true, 'procurement_request_id' => $procurement->id])
+      ->assertStatus(200)
+      ->assertJsonStructure(['saved', 'message']);
+  }
 }
