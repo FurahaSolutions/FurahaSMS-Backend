a1b508e4b55324841f1f16f9bdd0309ae1a41355
Owen Kelvin <owen@furahasolutions.tech>
2021-04-19 13:37:15 +0300

    Added class to test academic year timing

--- packages/okotieno/laravel-school-timetable/src/Controllers/TimeTableTimingsController.php
+++ packages/okotieno/laravel-school-timetable/src/Controllers/TimeTableTimingsController.php
@@ -13,7 +13,6 @@ class TimeTableTimingsController  extends Controller
     public function index(AcademicYear $academicYear, TimeTable $timeTable) {
         $template = TimeTableTimingTemplate::find($timeTable->time_table_timing_template_id);
         $response = $template->timings;
-
         return response()->json($response);
     }
 
