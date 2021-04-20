2db530c1dfe7e933d773f652886e5ca800e812d5
Owen Kelvin <owen@furahasolutions.tech>
2021-04-20 14:32:32 +0300

    Updated procurement request flow

--- packages/okotieno/laravel-procurements/src/routes.php
+++ packages/okotieno/laravel-procurements/src/routes.php
@@ -11,17 +11,20 @@ use Okotieno\Procurement\Controllers\ProcurementTenderFulfillmentController;
 use Okotieno\Procurement\Controllers\ProcurementVendorsController;
 
 Route::middleware(['auth:api', 'bindings'])->group(function () {
-  Route::prefix('api/procurements')->group(function () {
-    Route::apiResources([
-      'tenders/{procurementTender}/fulfilled' => ProcurementTenderFulfillmentController::class,
-      'tenders/{procurementTender}/bids' => ProcurementTenderBidsController::class,
-      'tenders' => ProcurementTenderController::class,
-      'vendors' => ProcurementVendorsController::class,
-      'requests/pending-tendering' => ProcurementRequestTenderController::class,
-      'requests/pending-approval' => ProcurementRequestApprovalController::class,
-      'requests' => ProcurementRequestController::class,
-      'item-categories' => ProcurementItemsCategoryController::class,
-    ]);
+  Route::prefix('api')->group(function () {
+
+    Route::prefix('procurements')->group(function () {
+      Route::apiResources([
+        'tenders/{procurementTender}/fulfilled' => ProcurementTenderFulfillmentController::class,
+        'tenders/{procurementTender}/bids' => ProcurementTenderBidsController::class,
+        'tenders' => ProcurementTenderController::class,
+        'vendors' => ProcurementVendorsController::class,
+        'requests/pending-tendering' => ProcurementRequestTenderController::class,
+        'requests/pending-approval' => ProcurementRequestApprovalController::class,
+        'item-categories' => ProcurementItemsCategoryController::class,
+      ]);
+    });
+    Route::apiResource('procurements', ProcurementRequestController::class);
     Route::get('my-requests', [ProcurementRequestApiController::class, 'myRequests']);
   });
 });
