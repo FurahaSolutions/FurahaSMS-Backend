77a0171dc8a75685a5304cf09cb3851216647c61
Owen Kelvin <owen@furahasolutions.tech>
2021-06-23 18:02:25 +0300

    added rule for array

--- packages/okotieno/laravel-school-exams/tests/ExamPapersQuestionTest.php
+++ packages/okotieno/laravel-school-exams/tests/ExamPapersQuestionTest.php
@@ -54,6 +54,23 @@ class ExamPapersQuestionTest extends TestCase
    * @test
    * @return void
    */
+  public function error_422_if_no_questions_provided()
+  {
+    Permission::factory()->state(['name' => 'create exam paper question'])->create();
+    $this->user->givePermissionTo('create exam paper question');
+    $this->actingAs($this->user, 'api')
+      ->postJson('/api/exam-papers/' . $this->examPaper->id . '/questions', [])
+      ->assertStatus(422);
+
+  }
+  /**
+   * POST /api/exam-papers/:exam-paper/questions
+   * @group exam-paper-questions
+   * @group post-request
+   * @test
+   * @return void
+   */
+
   public function authorised_users_can_create_exam_paper_questions()
   {
     Permission::factory()->state(['name' => 'create exam paper question'])->create();
